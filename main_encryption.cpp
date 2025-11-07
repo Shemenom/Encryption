2d487b7885bc55bde5fed3ddebbcf318c83ae41e
Shemenom <kksalakmhankk@inbox.ru>
2025-11-06 20:42:10 -0800

    Проверка времени выполнения

--- main_encryption.cpp
+++ main_encryption.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <chrono>
+#include <functional>
+#include "algorithm.h"
+
+// это не трогать !!!
+template<typename Func, typename... Args>
+auto Time(Func&& func, Args&&... args) {
+    auto start = std::chrono::high_resolution_clock::now();
+    
+    std::invoke(std::forward<Func>(func), std::forward<Args>(args)...);
+    
+    auto end = std::chrono::high_resolution_clock::now();
+    return std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+}
 
 int main() {
+    // здесь проверяйте свой код
 
-    return 0;
+
 }
